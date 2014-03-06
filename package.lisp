9caee2043816d93e61a6af895793350f497931f4
Shinmera <shinmera@tymoon.eu>
2014-03-06 23:55:16 +0100

    Updated export

--- package.lisp
+++ package.lisp
@@ -10,5 +10,44 @@
   (:use #:cl #:lquery #:alexandria #:closer-mop #:split-sequence)
   (:shadowing-import-from
    #:cl #:defmethod #:defgeneric #:standard-generic-function)
+  ;; symbols.lisp
   (:export
-   #:generate))
+   #:*symbol-object-transformers
+   #:define-symbol-transformer
+   #:define-easy-symbol-transformer
+   
+   #:symb-object
+   #:symb-symbol
+   #:symb-type
+   #:symb-variable
+   #:symb-function
+   #:symb-macro
+   #:symb-generic
+   #:symb-method
+   #:symb-class
+   #:symb-special
+   #:symb-constant
+   
+   #:symb-type
+   #:symb-scope
+   #:symb-qualifiers
+   #:symb-argslist
+   #:symb-docstring
+   
+   #:symbol-function-p
+   #:smybol-macro-p
+   #:symbol-generic-p
+   #:symbol-constant-p
+   #:smybol-special-p
+   #:symbol-class-p
+
+   #:symbol-objects
+   #:package-symbols)
+
+  ;; templating.lisp
+  (:export
+   #:*recognized-blocks*
+   #:process-block
+   #:define-block-processor
+   #:generate
+   #:process-symbol-object))
