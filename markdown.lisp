3f5cf179eb76a4ecc041e985180dd38d33aeee4a
Shinmera <shinmera@tymoon.eu>
2018-08-18 09:44:37 +0200

    Conditionally add 3bmd:*generate-header-ids*

--- markdown.lisp
+++ markdown.lisp
@@ -7,7 +7,10 @@
 (in-package #:org.shirakumo.staple)
 
 (define-source-compiler (:markdown "md") (input)
-  (let ((3bmd-code-blocks:*code-blocks* T))
+  (let ((3bmd-code-blocks:*code-blocks* T)
+        #.(if (find-symbol "*GENERATE-HEADER-IDS*" "3BMD")
+              `(3bmd:*generate-header-ids* T)
+              (gensym "Stub")))
     (with-output-to-string (out)
       (3bmd:parse-string-and-print-to-stream
        input out))))
